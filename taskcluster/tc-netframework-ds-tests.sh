7d94b8ac1919e15e2c2ae70f607cfa4869a5f9a7
Anton Yaroshenko <kpakadijl@hotmail.com>
2020-12-06 17:36:37 +0200

    dotnet tests fixes

--- taskcluster/tc-netframework-ds-tests.sh
+++ taskcluster/tc-netframework-ds-tests.sh
@@ -23,7 +23,7 @@ fi
 
 download_data
 
-install_nuget "${PROJECT_NAME}"
+install_nuget "${PROJECT_NAME}" "DeepSpeechConsole.exe"
 
 DS_BINARY_FILE="DeepSpeechConsole.exe"
 ensure_cuda_usage "$2"
@@ -32,5 +32,9 @@ run_netframework_inference_tests
 
 run_hotword_tests
 
+install_nuget "${PROJECT_NAME}" "DeepSpeechConsoleNetCore.exe"
+
 DS_BINARY_FILE="DeepSpeechConsoleNetCore.exe"
+ensure_cuda_usage "$2"
+
 run_netcore_inference_tests
